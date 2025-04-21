a1b0f45135b819f427d89df314267ed455e4ac18
unknown <rushik.sakamuri@gmail.com>
2025-04-21 14:23:10 -0400

    modified dashboard and added logout button

--- GreenQuest/app.py
+++ GreenQuest/app.py
@@ -81,5 +81,11 @@ def dashboard():
     
     return redirect(url_for('login'))
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
     app.run(debug=True)
